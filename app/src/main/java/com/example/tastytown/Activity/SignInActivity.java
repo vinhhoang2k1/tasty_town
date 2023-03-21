6811b6177b4474264929a7afe81a945ac7d562e5
Hoang Vinh <20200129@eaut.edu.vn>
2023-03-21 10:10:04 +0700

    make sign in screen

---
+++ app/src/main/java/com/example/tastytown/Activity/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.tastytown.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tastytown.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
--- app/src/main/java/com/example/tastytown/Activity/StartedActivity.java
+++ app/src/main/java/com/example/tastytown/Activity/StartedActivity.java
@@ -2,15 +2,26 @@ package com.example.tastytown.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.tastytown.R;
 
 public class StartedActivity extends AppCompatActivity {
-
+    TextView signInBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_started);
+        signInBtn = findViewById(R.id.start_sign_in_btn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartedActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
